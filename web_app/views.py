401e919570660122d6822518f1a7608eb62e653b
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2024-01-11 17:05:43 -0500

    took out unneeded code in login.html and changed title in html. Adding preference page functionality

--- web_app/views.py
+++ web_app/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, request
 from flask_login import login_required, current_user
+from .models import User, Movie
 
 views = Blueprint('views', __name__)
 
@@ -14,4 +15,16 @@ def access():
 @views.route('/prefer', methods=['POST', 'GET'])
 # @login_required
 def prefer():
+
+    if request.method == 'POST':
+        fav_movie1 = request.form.get('fav_movie1')
+        fav_movie2 = request.form.get('fav_movie2')
+        fav_movie3 = request.form.get('fav_movie3')
+        fav_genre1 = request.form.get('fav_genre1')
+        fav_genre2 = request.form.get('fav_genre2')
+        fav_genre3 = request.form.get('fav_genre')
+
+        if Movie.query.filter_by(title=fav_movie1).first() is None:
+            flash("Sorry, that movie is not in our registry, please try typing a different movie")
+
     return render_template('prefer.html', user=current_user)
