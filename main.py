05538fbbddb6151a8fb92dac9e351242d8c01f2e
Corinne <CorinneKogut@gmail.com>
2024-01-11 19:05:52 -0500

    commenting out load data for now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from web_app import create_app, db, ssh_tunnel
 from flask_migrate import Migrate
-from load_data import load_movies_data
 
 app = create_app()
 migrate = Migrate(app, db)
@@ -8,8 +7,7 @@ migrate = Migrate(app, db)
 # Initialize the database
 with app.app_context():
     db.create_all()
-    # Load
-    load_movies_data()
+
 
 # will only run the web server in this file
 if __name__ == "__main__":
