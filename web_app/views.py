e9e0562a0822abb99a259edc126e70f12b339055
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2024-01-11 21:17:52 -0500

    more ecode added for the preference route

--- web_app/views.py
+++ web_app/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, flash, request
 from flask_login import login_required, current_user
-from .models import User, Movie
+from .models import User, Movie, MovieGenre
 
 views = Blueprint('views', __name__)
 
@@ -15,18 +15,27 @@ def access():
 @views.route('/prefer', methods=['POST', 'GET'])
 # @login_required
 def prefer():
-
     if request.method == 'POST':
-        fav_movie1 = request.form.get('fav_movie1')
-        fav_movie2 = request.form.get('fav_movie2')
-        fav_movie3 = request.form.get('fav_movie3')
+        fav_mov1 = request.form.get('fav_mov1')
+        fav_mov2 = request.form.get('fav_mov2')
+        fav_mov3 = request.form.get('fav_mov3')
         fav_genre1 = request.form.get('fav_genre1')
         fav_genre2 = request.form.get('fav_genre2')
         fav_genre3 = request.form.get('fav_genre')
 
-        if Movie.query.filter_by(title=fav_movie1).first() is None:
-            flash("Sorry, that movie is not in our registry, please try typing a different movie")
+        # checking to see if the movie the user inputted is in the database
+        if Movie.query.filter_by(title=fav_mov1).first() is None:
+            flash("Sorry, the first movie you entered is not in our registry, please try typing a different movie")
+        elif Movie.query.filter_by(title=fav_mov2).first() is None:
+            flash("Sorry, the second movie you entered is not in our registry, please try typing a different movie")
+        elif Movie.query.filter_by(title=fav_mov3).first() is None:
+            flash("Sorry, the third movie you entered is not in our registry, please try typing a different movie")
+        elif MovieGenre.query.filter_by(genre=fav_genre1) is None:
+            flash("Your first entry for favorite genres does not exists in our records. please select another genre")
+        elif MovieGenre.query.filter_by(genre=fav_genre2) is None:
+            flash("Your second entry for favorite genres does not exists in our records. please select another genre")
+        elif MovieGenre.query.filter_by(genre=fav_genre3) is None:
+            flash("Your third entry for favorite genres does not exists in our records. please select another genre")
         else:
-            pass
-
+            new_user_fav = User.query.update()
     return render_template('prefer.html', user=current_user)
