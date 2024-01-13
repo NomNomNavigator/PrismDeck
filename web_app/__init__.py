216a5988fec136f155bf72ecd6d61afa24cb9f03
Corinne <CorinneKogut@gmail.com>
2024-01-13 14:31:26 -0500

    sync

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from sshtunnel import SSHTunnelForwarder
-from config import *
+from config1 import *
 
 
 db = SQLAlchemy()
