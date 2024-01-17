23417599a4b4094cb83bd558329d0a150cc39712
Joey8419 <jnelson297@gmail.com>
2024-01-16 22:11:06 -0500

    added code to rec.py

--- web_app/rec.py
+++ web_app/rec.py
@@ -1,12 +1,11 @@
 from flask import Blueprint, render_template, request, jsonify
 from pyspark.sql import SparkSession
-from pyspark.ml.recommendation import ALS, ALSModel
 from flask_login import current_user, login_required
 from .models import MovieRating, Movie
 from .recommendation_model import load_als_model, get_recommendations
 
-
 rec = Blueprint('rec', __name__)
+
 
 @rec.route('/get_recommendations', methods=['POST'])
 def get_recommendations():
@@ -20,3 +19,13 @@ def get_recommendations():
 
         # Extract user ID from the request data
         user_id = request.json['user_id']
+
+        # Get recommendations using the ALS model
+        result = get_recommendations(als_model, spark, user_id)
+
+        # Stop the spark session
+        spark.stop()
+
+        return jsonify({'recommendations': result})
+    except Exception as e:
+        return jsonify({'error': str(e)})
