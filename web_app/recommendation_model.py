41d46e13bddcd5cf08252b5147f977c04cd9af7c
Joey8419 <jnelson297@gmail.com>
2024-01-16 21:42:33 -0500

    added code to recommendation.py

--- web_app/recommendation_model.py
+++ web_app/recommendation_model.py
@@ -1,1 +1,12 @@
-does anything have to be done in these template files based on the __init__.py, rec
+from pyspark.ml.recommendation import ALSModel
+from pyspark.sql import SparkSession
+
+# Load  the pre-trained ALS model
+def load_als_model(model_path):
+    return ALS Model.load(model_path)
+
+
+# Use the ALS model to generate recommendations for a user
+def get_recommendations(model, spark, user_id, num_rec=10):
+    user_df = spark.createDataFrame([(user_id,)], ['userId'])
+    recommendations = model.recommendfor
